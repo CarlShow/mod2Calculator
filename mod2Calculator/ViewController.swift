dd91c959b5234cbf257a48acd129495b39382aa5
Carl <carl.show@yahoo.com>
2021-09-10 07:40:20 -0500

    Mini calculator--- it's pretty cool.

--- mod2Calculator/ViewController.swift
+++ mod2Calculator/ViewController.swift
@@ -8,12 +8,153 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var swapper: UISegmentedControl!
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var multButton: UIButton!
+    @IBOutlet weak var divButton: UIButton!
+    @IBOutlet weak var ansLabel: UILabel!
+    @IBOutlet weak var calculateButton: UIButton!
+    @IBOutlet weak var areaSwapper: UISegmentedControl!
+    var didSwap = false
+    var whichSwtich = [false, false, false]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        do {
+            let miniText = [NSAttributedString.Key.foregroundColor: UIColor.white]
+            swapper.setTitleTextAttributes(miniText, for: .selected)
+        }
+        addButton.layer.cornerRadius = 5
+        minusButton.layer.cornerRadius = 5
+        multButton.layer.cornerRadius = 5
+        divButton.layer.cornerRadius = 5
+        calculateButton.layer.cornerRadius = 5
+        ansLabel.layer.cornerRadius = 8
+        calculateButton.isHidden = true
+        areaSwapper.isHidden = true
     }
-
-
+    @IBAction func onSwap(_ sender: Any)
+    {
+        if didSwap == false
+        {
+            didSwap = true
+            calculateButton.isHidden = false
+            areaSwapper.isHidden = false
+            addButton.isHidden = true
+            minusButton.isHidden = true
+            multButton.isHidden = true
+            divButton.isHidden = true
+            textField2.isHidden = false
+            onAreaSwap((Any).self)
+        }
+        else
+        {
+            didSwap = false
+            calculateButton.isHidden = true
+            areaSwapper.isHidden = true
+            addButton.isHidden = false
+            minusButton.isHidden = false
+            multButton.isHidden = false
+            divButton.isHidden = false
+            textField2.isHidden = false
+            whichSwtich = [false, false, false]
+        }
+    }
+    @IBAction func onAreaSwap(_ sender: Any)
+    {
+        whichSwtich = [false, false, false]
+        whichSwtich[areaSwapper.selectedSegmentIndex] = true
+        print(whichSwtich)
+        if whichSwtich[0]
+        {
+            textField2.isHidden = false
+        }
+        else if whichSwtich[1]
+        {
+            textField2.isHidden = false
+        }
+        else
+        {
+            textField2.isHidden = true
+        }
+    }
+    @IBAction func add(_ sender: Any)
+    {
+        whichSwtich[0] = true
+        onCalc()
+        whichSwtich[0] = false
+    }
+    @IBAction func sub(_ sender: Any)
+    {
+        whichSwtich[1] = true
+        onCalc()
+        whichSwtich[1] = false
+    }
+    @IBAction func mult(_ sender: Any)
+    {
+        whichSwtich[2] = true
+        onCalc()
+        whichSwtich[2] = false
+    }
+    @IBAction func div(_ sender: Any)
+    {
+        onCalc()
+    }
+    
+    func onCalc()
+    {
+        if let var1 = Double(textField1.text!)
+        {
+            if let var2 = Double(textField2.text!)
+            {
+                if whichSwtich[0]
+                {
+                    ansLabel.text = String(var1 + var2)
+                }
+                else if whichSwtich[1]
+                {
+                    ansLabel.text = String(var1 - var2)
+                }
+                else if whichSwtich[2]
+                {
+                    ansLabel.text = String(var1 * var2)
+                }
+                else
+                {
+                    ansLabel.text = String(var1 / var2)
+                }
+            }
+        }
+    }
+    @IBAction func onFigure(_ sender: Any)
+    {
+        if whichSwtich[2]
+        {
+            areaOfCircle()
+        }
+        else if let var1 = Double(textField1.text!)
+        {
+            if let var2 = Double(textField2.text!)
+            {
+                if whichSwtich[0]
+                {
+                    ansLabel.text = String(var1*var2)
+                }
+                else
+                {
+                    ansLabel.text = String((var1*var2)/2)
+                }
+            }
+        }
+    }
+    func areaOfCircle()
+    {
+        if let var1 = Double(textField1.text!)
+        {
+            ansLabel.text = String(var1*3.14)
+        }
+    }
 }
 
